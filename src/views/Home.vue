950581ff80eda9974a9506c7cbab9dbc84c63078
Van Hiep <1712430@student.hcmus.edu.vn>
2020-02-19 21:39:45 +0700

    show list product & see detail

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,54 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h2>Products</h2>
+    <div class="product-container">
+      <div v-for="product in products" v-bind:key="product.id">
+        <router-link :to="{ name: 'product-detail', params:{id: product.id}}">
+          <div class="product-box">
+            <img :src="product.thumbnail" />
+            <div>
+              <h3>{{ product.name }}</h3>
+              <div v-html="product.description">{{ product.description }}</div>
+            </div>
+          </div>
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  components: {},
+  data() {
+    return {
+      products: this.$store.state.products
+    };
   }
 };
 </script>
+
+
+<style scoped lang="scss">
+.product-container {
+  .product-box {
+    border: 1px solid black;
+    border-radius: 10px;
+    margin: 10px;
+    padding: 10px;
+
+    text-align: left;
+
+    display: flex;
+    justify-content: flex-start;
+    img {
+      max-width: 200px;
+      padding: 10px;
+    }
+  }
+}
+</style>
---
+++ src/views/ProductDetail.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Product Detail</h1>
+    <img :src="product.thumbnail" alt />
+    <h3>{{ product.name }}</h3>
+    <div v-html="product.description"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    product() {
+      return this.$store.state.products.find(
+        pid => pid.id == this.$route.params.id
+      );
+    }
+  }
+};
+</script>
+
+<style scoped>
+img {
+  max-width: 50%;
+}
+</style>
